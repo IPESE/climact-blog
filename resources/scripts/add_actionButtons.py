0537daa2042264706cab06c165788af771f66665
jcferrei <joao.ferreiradasilva@epfl.ch>
2024-03-14 14:38:37 +0100

    start prod setup & addButttons in pre-render

---
+++ resources/scripts/add_actionButtons.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+import psutil
+
+# Function to add the script tag to an HTML file
+def add_script_tag_to_qmd(file_path):
+    # if "2023-10-25-create-new-post" in file_path:
+        with open(file_path, 'r') as file:
+            qmd_content = file.read()
+
+        sections = qmd_content.split('---')
+
+        if len(sections)>1:
+            if len(sections)>2 and '<script src="../../resources/scripts/actionButtons.js"></script>' in sections[2]:
+                # Find the end of the existing HTML block
+                end_of_block = sections[2].find('```', sections[2].find('```{=html}') + 1)
+                # Remove the existing HTML block
+                sections[2] = sections[2][end_of_block + 2:]
+                
+            script = "\n```{=html}\n<script src=\"../../resources/scripts/actionButtons.js\"></script>\n"
+            script= script + "```"
+
+            sections[2]=script+sections[2]
+
+            qmd_content = '---'.join(sections)
+
+            # Write the modified content back to the file
+            with open(file_path, 'w') as file:
+                file.write(qmd_content)
+
+# Function to copy chatbot.js to the _site directory
+def copy_script_to_site_directory(site_directory,filename):
+    script_source_path = './resources/scripts/'+filename
+    script_dest_path = os.path.join(site_directory, 'resources', 'scripts', filename)
+
+    # Create the directory if it doesn't exist
+    os.makedirs(os.path.dirname(script_dest_path), exist_ok=True)
+
+    if os.path.exists(script_source_path):
+        shutil.copy(script_source_path, script_dest_path)
+    else:
+        print(f'{filename} not found at {script_source_path}')
+
+# Function to recursively traverse the directory and add script tags
+def add_script_tags_to_directory(directory_path):
+    for root, _, files in os.walk(directory_path):
+        for file_name in files:
+            if file_name == 'index.qmd':
+                qmd_file_path = os.path.join(root, file_name)
+                print(qmd_file_path)
+                add_script_tag_to_qmd(qmd_file_path)
+
+def is_rendering():
+    all_pids = psutil.pids()
+
+    for pid in all_pids:
+        try:
+            process = psutil.Process(pid)
+            if "quarto" in process.cmdline()[1] and process.cmdline()[2]=="render":
+                return True
+        except:
+            pass
+    return True
+
+if is_rendering():
+    add_script_tags_to_directory('./posts')
+    copy_script_to_site_directory('./_site', "actionButtons.js")
--- resources/scripts/deploy.py
+++ resources/scripts/deploy.py
@@ -2,6 +2,7 @@ import os
 import re
 import shutil
 import subprocess
+import sys
 import time
 
 log_file_path = "quarto_logs.log" 
@@ -39,5 +40,54 @@ def trydeploy():
 
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
+
+def exclude_dev_posts(directory_path):
+    menuempty=True
+    # for every folder
+    for root, _, files in os.walk(directory_path):
+        # for every file found
+        for file_name in files:
+            # if its an index.qmd
+            if file_name.endswith('index.qmd'):
+                # if it's not already a hidden folder and file exists
+                file_path = os.path.join(root, file_name)
+                if not file_path.replace(directory_path+"/", "").startswith("_") and os.path.exists(file_path):
+                        with open(file_path, 'r') as file:
+                            qmd_content = file.read()
+
+                        sections = qmd_content.split('---')
+                        if len(sections)>1 and "prod: true" not in sections[1]:
+                            new_file_path = directory_path+"/_"+file_path.replace(directory_path+"/", "")
+                            new_file_path = new_file_path.replace("/"+file_name, "")
+                            file_path=file_path.replace("/"+file_name, "")
+                            print(file_path,new_file_path)
+                            shutil.move(file_path,new_file_path)
+                        else:
+                            menuempty=False
+    if menuempty:
+        with open(os.path.join(directory_path, "index.qmd"), 'w') as f:
+            f.write("")
+
+
+def add_prod_prefix(file_path):
+    with open("./_quarto.yml", 'r') as yaml_file:
+        yaml_content = yaml_file.read()
+
+    # Update the YAML content by replacing file paths
+    directory, file_name = os.path.split(file_path)
+    new_file_path = os.path.join(directory, "prod_"+file_name)
+    yaml_content = yaml_content.replace(file_path, new_file_path)
+
+    # Write the modified YAML content back to the file
+    with open("./_quarto.yml", 'w') as yaml_file:
+        yaml_file.write(yaml_content)
+
+def exclude_dev_parts():
+    print("Excluding unready posts")
+    exclude_dev_posts('./posts')
+    add_prod_prefix("./resources/scripts/add_actionButtons.py")
+
+if len(sys.argv)>1 and sys.argv[1]=="production":
+    exclude_dev_parts()
 
 trydeploy()
